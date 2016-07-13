022416186d466140948de6ed6f17a77d292751df
devjin0617 <devjin0617@gmail.com>
2016-07-13 23:49:54 +0900

    index.js - Write prototype code

---
+++ index.js
@@ -0,0 +1,54 @@
+
+function init(date) {
+
+    if(!date) {
+        return this.date = new Date();
+    }
+
+    if(Object.prototype.toString.call(date) === '[object Date]') {
+        return this.date = date;
+    }
+
+    if(typeof date === 'string') {
+        return this.dateString = date;
+    }
+
+}
+exports.init = init;
+
+function toDateString(date, format) {
+
+    if(!date) {
+        if(!this.date) {
+            console.log('error');
+            return;
+        }
+        date = this.date;
+    }
+
+    if(!format) {
+        format = 'yyyy.mm.ddT00:00:00+0900';
+    }
+
+    return this.dateString = date.toString();
+
+}
+exports.toDateString = toDateString;
+
+function toDate(dateString, format) {
+
+    if(!dateString) {
+        if(!this.dateString) {
+            console.log('error');
+            return;
+        }
+        dateString = this.dateString;
+    }
+
+    if(!format) {
+        format = 'yyyy.mm.ddT00:00:00+0900';
+    }
+
+    return this.date = new Date(dateString);
+}
+exports.toDate = toDate;
